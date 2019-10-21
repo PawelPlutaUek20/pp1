4861b649105bec05833b2317d340792e9d1012f5
s-115-23 <s-115-23@gumiforest.ae.krakow.pl>
2019-10-21 14:10:22 +0200

    zad 02

---
+++ 02-ControlStructures/02.py
@@ -0,0 +1,39 @@
+#x = 10
+#y = 23
+#if x>y:
+#    print(x, "jest większe od",y)
+#elif x==y:
+#    print(x, "jest rowne",y)
+#else:
+#    print(y, "jest większe od",x)
+
+#x= int(input("Podaj liczbe calkowitą: "))
+#if x%2!=0 and x>0:
+#    print(x, "jest zarówno dodatnia, jak i nieparzysta.")
+#else:
+#    print(x, "albo nie jest dodatnia albo jest parzysta")
+
+#Zadanie 14
+#wiek_psa=int(input("Podaj wiek psa w ludzkich latach:: "))
+#if wiek_psa<=2:
+#    print("Wiek psa w psich latach to", wiek_psa*10.5, "lata")
+#elif wiek_psa>2:
+#    print("Wiek psa w psich latach to", 21+(wiek_psa-2)*4, "lata")
+
+#Zadanie 13
+#x=5
+#y=2
+#if x>0 and y>0:
+#    print("Punkt P("+str(x)+","+str(y)+") znajduje się w pierwszej ćwiartce układu współrzędnych")
+#elif x<0 and y>0:
+#    print("Punkt P("+str(x)+","+str(y)+") znajduje się w drugiej ćwiartce układu współrzędnych")
+#elif x<0 and y<0:
+#    print("Punkt P("+str(x)+","+str(y)+") znajduje się w trzeciej ćwiartce układu współrzędnych")
+#elif x>0 and y<0:
+#    print("Punkt P("+str(x)+","+str(y)+") znajduje się w czwartej ćwiartce układu współrzędnych")
+#elif x==0 and y!=0:
+#    print("Punkt P("+str(x)+","+str(y)+") znajduje się na osi OY")
+#elif y==0 and x!=0:
+#    print("Punkt P("+str(x)+","+str(y)+") znajduje się na osi OX")
+#else:
+#    print("Punkt P("+str(x)+","+str(y)+") znajduje się on w początku układu współrzędnych")
