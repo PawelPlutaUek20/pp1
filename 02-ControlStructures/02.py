c7be664ea64c52babd4b87295af04d60f5f8841c
s-115-23 <s-115-23@gumiforest.ae.krakow.pl>
2019-10-21 14:38:00 +0200

    Zad 02

--- 02-ControlStructures/02.py
+++ 02-ControlStructures/02.py
@@ -14,7 +14,7 @@
 #    print(x, "albo nie jest dodatnia albo jest parzysta")
 
 #Zadanie 14
-#wiek_psa=int(input("Podaj wiek psa w ludzkich latach:: "))
+#wiek_psa=int(input("Podaj wiek psa w ludzkich latach: "))
 #if wiek_psa<=2:
 #    print("Wiek psa w psich latach to", wiek_psa*10.5, "lata")
 #elif wiek_psa>2:
@@ -37,3 +37,16 @@
 #    print("Punkt P("+str(x)+","+str(y)+") znajduje się na osi OX")
 #else:
 #    print("Punkt P("+str(x)+","+str(y)+") znajduje się on w początku układu współrzędnych")
+
+#Zadanie 15
+#x=int(input("Podaj liczbe: "))
+#for i in range(1,11):
+#    print(x, "x", i, "=", x*i)
+
+#Zadanie 22
+#x=[15,8,31,47,2,19]
+#y=[]
+#for i in x:
+#    if i%2!=0:
+#        y.append(i)
+#print("średnią arytmetyczną wszystkich liczb nieparzystych wynosi:",sum(y)/len(y))
