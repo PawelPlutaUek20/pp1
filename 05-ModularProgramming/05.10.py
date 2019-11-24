ac523daaf0402ef2bafa725d52a00d9408ffc988
unknown <pawelpluta58@gmail.com>
2019-11-24 15:50:44 +0100

    zad 05

--- 05-ModularProgramming/05.10.py
+++ 05-ModularProgramming/05.10.py
@@ -1,20 +1,6 @@
-from QuadraticEquation import *
-print('Równanie kwadratowe postaci:',end=' ')
-if b>0:
-    b=f'+{b}'
-elif b==0:
-    b=''
-if c>0:
-    c=f'+{c}'
-elif c==0:
-    c=''   
-if a==1:
-    print(f'x²{b}x{c}=0')
-else:
-    print(f'{a}x²{b}x{c}=0')
-if len(pierwiastki)==2:
-    print(f'Pierwiastki równania: x₁={x1}, x₂={x2}')
-elif len(pierwiastki)==1:
-    print(f'Pierwiastek równania: x₀={x0}')
-elif len(pierwiatki)==0:
-    print(f'Równanie nie ma pierwiastków')
+import QuadraticEquation as qe
+
+wspolczynniki = qe.czytajWspolczynniki()
+delta = qe.obliczDelte(wspolczynniki)
+pierwiastki = qe.obliczPierwiastki(wspolczynniki,delta)
+qe.wyswietlPierwiastki(wspolczynniki,pierwiastki)
--- 05-ModularProgramming/QuadraticEquation.py
+++ 05-ModularProgramming/QuadraticEquation.py
@@ -1,6 +1,5 @@
 # odczytaj współczynniki z klawiatury, zwraca tablicę współczynników
 def czytajWspolczynniki():
-    global a,b,c
     a=float(input('Podaj liczbe wspolczynnika a: '))
     if a == int(a):
         a=int(a)
@@ -10,21 +9,22 @@ def czytajWspolczynniki():
     c=float(input('Podaj liczbe wspolczynnika c: '))
     if c == int(c):
         c=int(c)
-    wspolczynniki = [a,b,c]
-    return wspolczynniki
+    return [a,b,c]
 
 # oblicz deltę
-def obliczDelte():
-    global delta
-    a,b,c=czytajWspolczynniki()
-    delta = b**2-4*a*c
-    return delta
+def obliczDelte(wspolczynniki: list):
+    a = wspolczynniki[0]
+    b = wspolczynniki[1]
+    c = wspolczynniki[2]
+    return b**2-4*a*c
 
 # wyznacz pierwiastki równania - zwraca tablicę pierwiastków
 # (jeden lub dwa elementy) lub pustą tablicę, jeśli delta < 0
-def obliczPierwiastki():
-    global x1, x2, x0, pierwiastki
-    obliczDelte()
+def obliczPierwiastki(wspolczynniki: list, delta: float):
+    a = wspolczynniki[0]
+    b = wspolczynniki[1]
+    c = wspolczynniki[2]
+    
     if delta>0:
         x1 = (-b-(delta)**(1/2))/(2*a)
         x2 = (-b+(delta)**(1/2))/(2*a)
@@ -32,22 +32,36 @@ def obliczPierwiastki():
             x1=int(x1)
         if x2 == int(x2):
             x2=int(x2)
-        pierwiastki = [x1,x2]
-        return pierwiastki
+        return [x1,x2]
     elif delta==0:
         x0 = (-b)/(2*a)
         if x0==int(x0):
             x0=int(x0)
-        pierwiastki = [x0]
-        return pierwiastki
+        return [x0]
     else:
-        pierwiastki=[]
-        return pierwiastki
+        return []
 
 # wyświetl wyznaczone pierwiastki równania kwadratowego
-def wyswietlPierwiastki():
-    miejscaZerowe=[]
-    for i in obliczPierwiastki():
-        miejscaZerowe.append(i)
-    print(miejscaZerowe)
-obliczPierwiastki()
+def wyswietlPierwiastki(wspolczynniki:list,pierwiastki: list):
+    a = wspolczynniki[0]
+    b = wspolczynniki[1]
+    c = wspolczynniki[2]
+    print('Równanie kwadratowe postaci:',end=' ')
+    if b>0:
+        b=f'+{b}'
+    elif b==0:
+        b=''
+    if c>0:
+        c=f'+{c}'
+    elif c==0:
+        c=''   
+    if a==1:
+        print(f'x²{b}x{c}=0')
+    else:
+        print(f'{a}x²{b}x{c}=0')
+    if len(pierwiastki)==2:
+        print(f'Pierwiastki równania: x₁={pierwiastki[0]}, x₂={pierwiastki[1]}')
+    elif len(pierwiastki)==1:
+        print(f'Pierwiastek równania: x₀={pierwiastki[0]}')
+    elif len(pierwiastki)==0:
+        print(f'Równanie nie ma pierwiastków')
