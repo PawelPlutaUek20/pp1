cbb9b4d4ea43efe5f0bbdf0f1923a789cc086723
s-115-25 <s-115-25@gumiforest.ae.krakow.pl>
2020-01-20 14:25:27 +0100

    zad 10

--- 10-SoftwareTesting/sum_numbers.py
+++ 10-SoftwareTesting/sum_numbers.py
@@ -1,15 +1,22 @@
+import math
 # Sumuje liczby nturalne parzyste z przedziału <from_n,to_n>
 def sum_even(from_m,to_n):
     sum = 0
-    for i in range(from_m,to_n):
-        if i%2 == 0: # liczba parzysta
+    if (type(from_m) != int and type(from_m) != float) or (type(to_n) != int and type(to_n) != float):
+        return 0
+    for i in range(math.ceil(from_m),math.floor(to_n+1)):
+        if to_n+1<0 :
+            return 0
+        if from_m <0 and from_m<to_n+1:
+            from_m+=1
+        elif i%2 == 0: # liczba parzysta
             sum += i
     return sum
 
 def main():
-    m = 1
-    n = 5
-    print(f"Suma liczb naturalnych parzystych z przedziału <{m},{n}> wynosi {sum_even(1,5)}")
+    m = "jeden"
+    n = "osiem"
+    print(f"Suma liczb naturalnych parzystych z przedziału <{m},{n}> wynosi {sum_even(m,n)}")
 
 if __name__ == "__main__":
     main()
--- 10-SoftwareTesting/test_sum_numbers.py
+++ 10-SoftwareTesting/test_sum_numbers.py
@@ -27,7 +27,29 @@ class TestSumEven(unittest.TestCase):
         n = 2
         result = sum_even(m,n)
         self.assertEqual(result, 0)
-
+        
+    def test_sum_even_nm(self):
+        # Testuj sumę liczb naturalnych parzystych 
+        # z przedziału m,n, gdzie m lub n są mniejsze od 0
+        m = -5
+        n = 10
+        result = sum_even(m,n)
+        self.assertEqual(result, 30)
+        
+    def test_sum_even_nm(self):
+        # Testuj sumę liczb naturalnych parzystych 
+        # z przedziału m,n, gdzie m lub n są mniejsze od 0
+        m = 2.123
+        n = 6.23
+        result = sum_even(m,n)
+        self.assertEqual(result, 10)
+    def test_sum_even_nm(self):
+        # Testuj sumę liczb naturalnych parzystych 
+        # z przedziału m,n, gdzie m lub n są mniejsze od 0
+        m = "jeden"
+        n = "osiem"
+        result = sum_even(m,n)
+        self.assertEqual(result, 0)
 
 
 if __name__ == '__main__':
